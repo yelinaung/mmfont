63def14efac2a5aae024e12dc8c54f50df2a7119
Ye Lin Aung <me@yelinaung.com>
2014-07-30 12:35:33 +0630

    MiniTest::Unit::TestCase is now Minitest::Test.

--- test/mmfont_test.rb
+++ test/mmfont_test.rb
@@ -1,7 +1,7 @@
 require "minitest/autorun"
 require "mmfont"
 
-class MmfontTest < MiniTest::Unit::TestCase
+class MmfontTest < Minitest::Test
   # unicode converting test
   def test_convert_uni512zg1
     assert_equal "မဂၤလာပါ", Mmfont::Converter.new.uni512zg1("မင်္ဂလာပါ")
